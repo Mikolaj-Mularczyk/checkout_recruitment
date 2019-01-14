e1e2a72204a088d12bd0397f1636b78217abc255
Mikolaj-Mularczyk <mularczyk.mikolaj@gmail.com>
2019-01-14 16:28:26 +0100

    Update BasketController.cs

--- BasketManager/Controllers/BasketController.cs
+++ BasketManager/Controllers/BasketController.cs
@@ -14,6 +14,7 @@ namespace BasketManager.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Produces("application/json")]
+    [Authorize(Policy = "ApiUser", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class BasketController : ControllerBase
     {
         private readonly IBasketService basketService;
